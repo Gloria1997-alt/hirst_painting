9d741ef2fd670371da2ba1dfca27107830a19249
Gloria Imoro <gloriaimoro@Glorias-MacBook-Air.local>
2023-05-08 13:15:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+# import colorgram
+#
+# rgb_colors = []
+# colors = colorgram.extract('image.jpg', 30)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+
+# print(rgb_colors)
+
+import turtle as turtle_module
+import random
+
+turtle_module.colormode(255)
+tim = turtle_module.Turtle()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+
+color_list = [(239, 241, 246), (233, 244, 239), (245, 235, 242), (242, 239, 228), (22, 30, 46), (140, 173, 200), (36, 105, 148),
+              (205, 160, 112), (226, 210, 99), (140, 29, 62)]
+
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+
+
+screen = turtle_module.Screen()
+screen.exitonclick()
